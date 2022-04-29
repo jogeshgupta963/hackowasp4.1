56adf64c3a959cba89caf629ac061bde7cde32d1
jogeshgupta <jogeshgupta963@gmail.com>
2022-04-29 11:30:24 +0530

    init commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const Discord = require('discord.js')
 const axios =require("axios")
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+
+
+client.on("ready",()=>{
+
+    console.log(`logged in as ${client.user.tag}`)
+})
+
+cient.on("messageCreate", async(msg)=>{        
+    
+
+})
+client.login(process.env.botToken)
