691ecd300885c2decf146f0e7833af0c48f79b6d
prishitasinghal <98651348+prishitasinghal@users.noreply.github.com>
2022-04-30 17:49:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ client.on("ready",()=>{
 function joke(){
     return "U r"
 }
+function getData () { 
+axios.get("https://zenquotes.io/api/random").then(
+  response=>{
+    console.log(response);
+  });
+  
+  
+};
 
 client.on("messageCreate", async(msg)=>{        
     
@@ -43,12 +51,15 @@ client.on("messageCreate", async(msg)=>{
     else if(msg.content && msg.author.bot !=true){
             msg.reply(`1.Movies 2.Jokes`)
         }
-    else if(msg.content =="heys bitches"){
-           msg.reply("aint your bitch")
-    }
-       
+   
 
 })
+ client.on("messageCreate",async(msg1)=>{
+  if(msg1.content==="heys bitches"){
+    msg1.reply("ain't your bitch")
+    
+  }
+ })
 
 
 client.login(process.env.botToken)
